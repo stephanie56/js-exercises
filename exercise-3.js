fb2c7e820d60048696dccefe3158883671f4d4eb
Communications <Communications>
2018-02-07 11:48:34 -0500

    Completed exercise 3

--- exercise-3.js
+++ exercise-3.js
@@ -3,7 +3,9 @@
 const numbers = [1, 2, 3, 4, 5];
 
 const map = (array, fn) => {
-  // your solution here
+  return array.reduce((prev, currNum) => {
+    prev.concat(fn(currNum));
+  }, []);
 };
 
 const double = x => x * 2;
