571ce5c1144c832c56d41ba7ba99e3010b4612e9
Stephanie Zeng <stephanie56@users.noreply.github.com>
2018-02-08 13:05:26 -0500

    Update recursion solution

--- exercise-4.js
+++ exercise-4.js
@@ -5,6 +5,28 @@
 
 const numbers = [1, 2, 3, 4, 5];
 
+const reduce = (array, fn, init) => {
+  if(array.length === 1) {
+    return fn(init, array[0]);
+  } else {
+    const smChunk = array.slice(0, 1);
+    const lgChunk = array.slice(1);
+    return reduce(lgChunk, fn, reduce(smChunk, fn, init));
+  }
+};
+
+// How it works
+// [1], [2, 3, 4, 5]
+// [1], [2], [3, 4, 5]
+// [1], [2], [3], [4, 5]
+// [1], [2], [3], [4], [5]
+
+const sum = (acc, val) => acc + val;
+
+console.log(reduce(numbers, sum, 0)); // 15
+
+module.exports = reduce;
+
 // Solution 1: not using recursion
 // const reduce = (array, fn, init) => {
 //   let acc = init;
@@ -12,20 +34,15 @@ const numbers = [1, 2, 3, 4, 5];
 //     acc = fn(acc, num);
 //   });
 //
-//   return total;
+//   return acc;
 // };
 
-const reduce = (array, fn, init) => {
-  let currIdx = array.length - 1;
-  if(currIdx <= 0) {
-    return fn(init, array[0]);
-  } else {
-    return fn(reduce(array.slice(0, currIdx), fn, init), array[currIdx--]);
-  }
-};
-
-const sum = (acc, val) => acc + val;
-
-console.log(reduce(numbers, sum, 0)); // 15
-
-module.exports = reduce;
+// Solution 2
+// const reduce = (array, fn, init) => {
+//   let currIdx = array.length - 1;
+//   if(currIdx <= 0) {
+//     return fn(init, array[0]);
+//   } else {
+//     return fn(reduce(array.slice(0, currIdx), fn, init), array[currIdx--]);
+//   }
+// };
