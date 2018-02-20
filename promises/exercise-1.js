9232f4f56324f0dc88fa9ce8e5510c2d01e5cd92
Purvi Kanal <purvikanal92@gmail.com>
2018-02-20 16:44:57 -0500

    commit
    promises examples

---
+++ promises/exercise-1.js
@@ -0,0 +1,12 @@
+require("es6-promise");
+
+// Create a promise. Have it fulfilled with a value of 'FULFILLED!' in executor after a delay of 300ms, using setTimeout.
+// Then, print the contents of the promise after it has been fulfilled by passing console.log to then.
+
+const promise = new Promise(function(resolve, reject) {
+  // Your solution here
+});
+
+const onResolve = value => {
+  // Your solution here
+};
